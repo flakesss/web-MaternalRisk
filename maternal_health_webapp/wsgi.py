598206c5406a09be891cead3ac14bbb78ee86ec5
Hatta Dwi Putranto <100501091+flakesss@users.noreply.github.com>
2024-12-02 16:22:36 +0700

    Update wsgi.py

--- maternal_health_webapp/wsgi.py
+++ maternal_health_webapp/wsgi.py
@@ -1,10 +1,5 @@
 # wsgi.py
 
-from serverless_wsgi import handle_request
 from api.index import app
 
-def handler(event, context):
-    return handle_request(app, event, context)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# Vercel akan mencari variabel 'app' dalam wsgi.py sebagai entry point
